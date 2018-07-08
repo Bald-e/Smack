ddc2469613ce73390dec8b1d27d212a409a14190
Yevgeniy Craighead <ycraighead@webroot.com>
2018-07-08 14:48:59 -0600

    Lesson 2

--- app/src/main/java/com/example/ycraighead/smack/MainActivity.kt
+++ app/src/main/java/com/example/ycraighead/smack/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.ycraighead.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -18,10 +15,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -38,5 +31,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun loginBtnNavClicked (view: View) {
 
+    }
+
+    fun addChannelClicked (view: View){
+
+    }
+
+    fun sendMessageBtnClicked (view: View){
+
+    }
 }
